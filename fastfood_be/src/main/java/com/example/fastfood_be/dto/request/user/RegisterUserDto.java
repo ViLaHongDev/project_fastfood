9b1b60007f6c07aff5eaf03c50178e19bfd6436d
LaHongVi <vixhoongla@gmail.com>
2024-10-24 11:00:13 +0700

    fix

--- fastfood_be/src/main/java/com/example/fastfood_be/dto/request/user/RegisterUserDto.java
+++
@@ -1,6 +0,0 @@
-package com.example.fastfood_be.dto.request.user;
-
-public class RegisterUserDto {
-    private String email;
-    private String password;
-}
--- fastfood_be/src/main/java/com/example/fastfood_be/exception/GlobalExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.example.fastfood_be.exception;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-
-}
